9f802d223142fdebe03ad69d8c24f4aeb7eeac17
Alex de Kruijff <alex.de.kruijff@mazarineblue.org>
2026-02-06 18:05:12 +0100

    Refactored code. And fixed the calculation of the surface of the triangle.

--- 00 - TileCost Calculator/Program.cs
+++ 00 - TileCost Calculator/Program.cs
@@ -9,45 +9,32 @@
 
         static void Main(string[] args)
         {
-            SecondTask(args);
+            int selection = GetShapeSelection(args);
+
+            int surface = 0;
+            switch (selection)
+            {
+                case OPTION_RECTANGLE:
+                    surface = PerformCalculationForRectangle();
+                    break;
+                case OPTION_TRIANGLE:
+                    surface = PerformCalculationForTriangle();
+                    break;
+            }
+            WriteCost(surface);
         }
 
-        static void FirstTask(string[] args)
-        {
-            Console.WriteLine("Please enter the width:");
-            int width = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine();
-
-            Console.WriteLine("Please enter the length:");
-            int length = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine();
-
-            int surface = width * length;
-            int cost = surface * PRICE_PER_HOUR / SQUARE_FEET_PER_HOUR;
-            Console.WriteLine($"The surface is: {surface}");
-            Console.WriteLine($"The cost is: ${cost}");
-        }
-
-        static void SecondTask(string[] args)
+        static int GetShapeSelection(string[] args)
         {
             Console.WriteLine("Please pick a shape:");
             Console.WriteLine($"{OPTION_RECTANGLE} - Rectangle");
             Console.WriteLine($"{OPTION_TRIANGLE} - Triangle");
             int selection = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine();
-
-            switch (selection)
-            {
-                case OPTION_RECTANGLE:
-                    PerformCalculationForRectangle();
-                    break;
-                case OPTION_TRIANGLE:
-                    PerformCalculationForTriangle();
-                    break;
-            }
+            return selection;
         }
 
-        static void PerformCalculationForRectangle()
+        static int PerformCalculationForRectangle()
         {
             Console.WriteLine("Please enter the width:");
             int width = Convert.ToInt32(Console.ReadLine());
@@ -57,28 +44,27 @@
             int length = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine();
 
-            int surface = width * length;
+            return width * length;
+        }
+
+        static int PerformCalculationForTriangle()
+        {
+            Console.WriteLine("Please enter the width:");
+            int width = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+
+            Console.WriteLine("Please enter the length:");
+            int length = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+
+            return width * length / 2;
+        }
+
+        static void WriteCost(int surface)
+        {
             int cost = surface * PRICE_PER_HOUR / SQUARE_FEET_PER_HOUR;
             Console.WriteLine($"The surface is: {surface}");
             Console.WriteLine($"The cost is: ${cost}");
         }
-
-        static void PerformCalculationForTriangle()
-        {
-            Console.WriteLine("Please enter the width:");
-            int width = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine();
-
-            Console.WriteLine("Please enter the length:");
-            int length = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine();
-
-            int surface = width * length;
-            int cost = surface * PRICE_PER_HOUR / SQUARE_FEET_PER_HOUR / 2;
-            Console.WriteLine($"The surface is: {surface}");
-            Console.WriteLine($"The cost is: ${cost}");
-        }
-
-        // At this point I would refactor
     }
 }
